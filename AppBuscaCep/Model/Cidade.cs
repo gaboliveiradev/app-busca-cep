f26ab0ce051b619bc53a1fab60c387265f8da061
gaboliveiradev <g38789138@gmail.com>
2023-02-10 14:32:32 -0300

    criando as propriedades da classe model Cidade

--- AppBuscaCep/Model/Cidade.cs
+++ AppBuscaCep/Model/Cidade.cs
@@ -6,5 +6,10 @@ namespace AppBuscaCep.Model
 {
     public class Cidade
     {
+        public int id_cidade { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public int codigo_ibge { get; set; }
+        public int ddd { get; set; }
     }
 }
